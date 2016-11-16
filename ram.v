d298329c9a3a0fa26e657c963d6e5f774b8ed387
yLoiSiah <louisesiah19@gmail.com>
2016-11-16 11:44:23 +0800

    edit REG current_WR_status, last_WR_status; from register type to wire type

--- ram.v
+++ ram.v
@@ -1,21 +1,39 @@
-module ram(clock, reset, write1_read0, data_in, data_out, data_out_by_bit);
- 
-	parameter data_size = 16;
-	input clock, reset, write1_read0;
-	input [data_size-1:0]data_in;
-	output reg [data_size-1:0]data_out;
-	// output reg [4095:0]data_out; no much pin
-	output reg data_out_by_bit;
-	
+module ram(clock, reset, write1_read0, data_in, data_out_display, address_display, byte_counter_display, status_change);
 
+parameter data_size = 16;
+input clock, reset, write1_read0;
+input [data_size-1:0]data_in;
+output wire [data_size-1:0]data_out_display;
+// output reg [4095:0]data_out; no much pin
+// output reg data_out_by_bit;
+output wire[4:0]address_display;
+output wire[7:0]byte_counter_display;
+output wire status_change;
+
+reg [data_size-1:0]data_out;
 reg [4095:0]register[31:0];
 reg [4095:0]temp_data;
-reg [4:0]address;
 reg [7:0] byte_counter; //256 (8 bit) * data (16 bit)
 reg [12:0] bit_counter; //count to 4096 bits
+reg [4:0]address; // address_for_bit;
 
-reg [11:0]MSB_bit,LSB_bit;
+wire current_WR_status, last_WR_status;
+wire change_WR_status_found;
 
+/************************** wiring output ***************/
+assign data_out_display = data_out;
+
+/****** taped out signals for checking purpose **************/
+assign status_change = change_WR_status_found;
+assign address_display = address;
+assign byte_counter_display = byte_counter;
+
+/****** checking read write values *****************/
+assign current_WR_status = write1_read0;
+assign last_WR_status = current_WR_status;
+assign change_WR_status_found = current_WR_status ^ last_WR_status;
+
+/********************* program start here **********************/
 always@(posedge clock or negedge reset)
 begin
 	if(!reset)
@@ -24,46 +42,46 @@ begin
 	end
 	else
 	begin
-		temp_data <={data_in, temp_data[4095:data_size]};
+		temp_data <={data_in[data_size-1:0], temp_data[4095:data_size]};
 	end
 end
 
-always@(posedge clock or negedge reset)
+
+/********** control of increment of address, byte_counter, bit_counter *********/
+always@(posedge clock or negedge reset or posedge change_WR_status_found)
 begin
-	if(!reset)
+	if(!reset | change_WR_status_found)
 	begin
 		address <= 0;
+//		address_for_bit <= 0;
 		bit_counter <= 0;
 		byte_counter <= 0;
+
 	end
 	else
 	begin
 		if(byte_counter < 255)
 		begin
 			byte_counter <= byte_counter + 1;
-			MSB_bit <= (byte_counter + 1) * 16 -1;
-			LSB_bit <= byte_counter * 16 - 1;
-		end
-		else
-		begin
-			byte_counter <= 0;
-			MSB_bit <= 15;
-			LSB_bit <= 0;
-		end
-		
-		if(!write1_read0)
-		begin
-			address <= 0;
-			if(bit_counter < 15'd4096)
-				bit_counter <= bit_counter + 1;
-			else
-				bit_counter <= 0;
-		end
-		else
-		if(address < 5'd32)
-			address <= address + 1;
-		else
 			address <= address;
+		end
+		else
+		begin
+			address <= address + 1;
+			byte_counter <= 0;
+		end
+
+//		if(bit_counter < 15'd4096)
+//		begin
+//			bit_counter <= bit_counter + 1;
+//			address_for_bit <= address_for_bit;
+//		end
+//		else
+//		begin
+//			bit_counter <= 0;
+//			address_for_bit <= address_for_bit + 1;
+//		end
+
 	end
 end
 
@@ -76,8 +94,8 @@ begin
 	end
 	else	//read
 	begin
-		data_out <= {register[address][MSB_bit-:16 ]};
-		// data_out_by_bit <= regi`ster[address][bit_counter];
+		data_out <= {register[address][((byte_counter + 1) * 16 -1)-:16]};
+//		data_out_by_bit <= register[address_for_bit][bit_counter];
 	end
 end
 
--- ram_tb.v
+++ ram_tb.v
@@ -1,78 +1,102 @@
 module ram_tb();
 
-ram #.(data_size(16)) ram1 (clock, reset, write1_read0, data_in, data_out, data_out_by_bit);
-	reg clock, WE;
-	reg [4:0]address;
-	reg [7:0]Input;
+parameter data_size = 16;
 
+//declares input as register, output as wire
+reg clock, reset, write1_read0;
+reg [data_size-1:0]data_in;
 //output wire
-	wire [7:0]Output;
-  
-//temp storage
-  // reg [memorySize - 1:0] location; 
-  integer i, j;
-  
-ram ram1 (clock, WE, address, Input, Output);
+wire [data_size-1:0]data_out;
+wire [4:0]address;
+wire [7:0]byte_counter;
+wire [4:0]MSB_bit_display;
+wire status_change;
 
-// initial 
- // begin 
-   // reset <= 0;
-   // @(posedge clock);
-   // @(negedge clock);
-   // reset <= 1;
- // end 
+ram #(.data_size(data_size)) ram1 (clock, reset, write1_read0, data_in, data_out, address, byte_counter, status_change);
+
+//-------------------parameter---------------------------
+reg [7:0] i, j;
+
 
 initial
+ begin
+   reset <= 0;
+   @(posedge clock);
+   @(negedge clock);
+   reset <= 1;
+ end
+
+//.................clock pulse generator.......................
+always #1 clock = !clock;
+
+/**************** main Block *******************************/
+initial
+begin
+initialise();
+#3 writeAndRead();
+$finish;
+end
+
+/* initial
   begin
-  clock = 0;
-  forever #1 clock = ~clock;
-  end
-
-initial 
-  begin 
-  #2 writeAndRead();
-  // for( location = 0; location < 32; location = location + 1)
-    // begin
-    // @(negedge clock); Input = 0; WE = 0; address = location;
-    // end
-  end
-
-initial 
-  begin 
   $display("clock| WE | address | Input | Output | time");
   $monitor("%b  | %b |  %d   | %d   | %d  | %d", clock, WE, address, Input, Output, $time);
-  end
+  end */
 
-  
-  
-//*************************
+
+
+/************************* Task ************************/
+task initialise;
+begin
+	data_in = 0;
+	clock = 0;
+	write1_read0 = 1;
+end
+endtask
+
 
 task writeAndRead;
 begin
- 
-  for(i = 0; i < 2; i = i + 1)
+  for(i = 0; i < 4; i = i + 1)
     begin
-    @(posedge clock)
-    if(!i) WE = 1;
-    else   WE = 0;
-    for( j = 0; j < 32; j = j + 1)
-      begin
-       if(!i) begin @(negedge clock); address = j; Input = j + 1; end
-       else begin @(negedge clock); address = j; Input = j + 4; end
-      end
+		@(posedge clock)
+		if(i<2) write1_read0 = 1; //write mode
+		else   write1_read0 = 0; //read mode
+		for( j = 0; j < 255; j = j + 1)
+		begin
+			//write mode
+			if(i<2) begin @(negedge clock); data_in = {i,j}; /*$display("i = %d, j = %b, j[(i+3)-:2] = %b\n", i, j, j[(i+3)-:2]);*/ end
+			//read mode
+			else begin @(negedge clock);  /*end*/ checking(i,j, data_out, address); end
+		end
     end
 end
 endtask
 
-// task read;
-// // input [4:0] addressR;
+reg [7:0]correct_row_index;
+reg [15:0]expect_data;
 
-// begin
- 
-    // begin
-      // #2 address = location; 
-    // end
-// end
-// endtask
+task checking;
+	input [7:0]row_index;
+	input [7:0]column_index;
+	input [15:0]data_input;
+	input [4:0]address;
+	begin
+		correct_row_index = row_index-2;
+		expect_data = {correct_row_index, column_index};
+		if(address != correct_row_index && byte_counter != column_index)
+		begin
+			$display("!!! \n expect address = %d, actual address = %d \n expect byte = %d, actual byte = %d",
+					address, correct_row_index, column_index, byte_counter);
+		end
+		else
+		begin
+			if(data_input != expect_data)
+				$display(" -_-!!! \n expect_data = %d, actual data = %d", expect_data, data_input);
+			else
+				$display("^_^\n");
+		end
+	end
+endtask
 
 endmodule
